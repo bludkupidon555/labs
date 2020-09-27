e901717304d0589892d3e147260168a67ce3c7a7
bludkupidon555 <ivan.turubar@gmail.com>
2020-09-27 17:38:41 +0300

    lab-02

--- lab-02/lab-02.lua
+++ lab-02/lab-02.lua
@@ -24,25 +24,89 @@ function ui.AddB:on_clicked(...)
 end
 
 function ui.SubbtractB:on_clicked(...)
-	a=tonumber(ui.NumberA.text)
-	b=tonumber(ui.NumberB.text)
-	ui.ResultTB.label = a-b;
+	if (type(tonumber(ui.NumberA.text))=='number' and type(tonumber(ui.NumberB.text))=='number')
+	then 
+		a=tonumber(ui.NumberA.text)
+		b=tonumber(ui.NumberB.text)
+		ui.ResultTB.label = a-b;
+	else
+		ui.ResultTB.label = "x_X_x_Error_x_X_x";
+	end
 end
 
 function ui.MultiplyB:on_clicked(...)
-	a=tonumber(ui.NumberA.text)
-	b=tonumber(ui.NumberB.text)
-	ui.ResultTB.label = a*b;
+	if (type(tonumber(ui.NumberA.text))=='number' and type(tonumber(ui.NumberB.text))=='number')
+	then 
+		a=tonumber(ui.NumberA.text)
+		b=tonumber(ui.NumberB.text)
+		ui.ResultTB.label = a*b;
+	else
+		ui.ResultTB.label = "x_X_x_Error_x_X_x";
+	end
 end
 
 function ui.DivideB:on_clicked(...)
-	a=tonumber(ui.NumberA.text)
-	b=tonumber(ui.NumberB.text)
-	if(b==0)
-	then ui.ResultTB.label = "x_X_x_Error_x_X_x";
+	if (type(tonumber(ui.NumberA.text))=='number' and type(tonumber(ui.NumberB.text))=='number')
+	then 
+		a=tonumber(ui.NumberA.text)
+		b=tonumber(ui.NumberB.text)
+		if(b==0)
+		then
+			ui.ResultTB.label = "x_X_x_Error_x_X_x";
+		else
+			ui.ResultTB.label = a/b;
+		end
 	else
- 	ui.ResultTB.label = a/b;
+		ui.ResultTB.label = "x_X_x_Error_x_X_x";
 	end
+end
+
+function ui.sinB:on_clicked(...)
+
+	if (type(tonumber(ui.NumberA.text))=='number')
+	then 
+		a=math.sin(math.rad(tonumber(ui.NumberA.text)));
+		ui.ResultTB.label = a;
+	else
+		ui.ResultTB.label = "x_X_x_Error_x_X_x";
+	end
+
+end
+
+function ui.cosB:on_clicked(...)
+
+	if (type(tonumber(ui.NumberA.text))=='number')
+	then 
+		a=math.cos(math.rad(tonumber(ui.NumberA.text)));
+		ui.ResultTB.label = a;
+	else
+		ui.ResultTB.label = "x_X_x_Error_x_X_x";
+	end
+
+end
+
+function ui.tgB:on_clicked(...)
+
+	if (type(tonumber(ui.NumberA.text))=='number')
+	then 
+		a=math.tan(math.rad(tonumber(ui.NumberA.text)));
+		ui.ResultTB.label = a;
+	else
+		ui.ResultTB.label = "x_X_x_Error_x_X_x";
+	end
+
+end
+
+function ui.ctgB:on_clicked(...)
+
+	if (type(tonumber(ui.NumberA.text))=='number')
+	then 
+		a=math.cos(math.rad(tonumber(ui.NumberA.text)))/math.sin(math.rad(tonumber(ui.NumberA.text)));
+		ui.ResultTB.label = a;
+	else
+		ui.ResultTB.label = "x_X_x_Error_x_X_x";
+	end
+
 end
 
 gtk.main()
