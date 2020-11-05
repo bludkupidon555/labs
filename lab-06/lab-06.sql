abf6e8c7837e1455f6c1531b88f6e2f7cc6638e5
bludkupidon555 <ivan.turubar@gmail.com>
2020-11-05 14:38:37 +0300

    lab-06

---
+++ lab-06/lab-06.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS employeeList;
+CREATE TABLE employeeList (Name TEXT);
+
+INSERT INTO employeeList VALUES ('Rublev Dmitriy');
+INSERT INTO employeeList VALUES ('Turubar Ivan');
+INSERT INTO employeeList VALUES ('Prilipchanu Daniil');
+INSERT INTO employeeList VALUES ('Zinko Danil');
+INSERT INTO employeeList VALUES ('Pogodin Petr');
+INSERT INTO employeeList VALUES ('Krivosheev Alexey');
+INSERT INTO employeeList VALUES ('Arthas');
+INSERT INTO employeeList VALUES ('Lich King');
+INSERT INTO employeeList VALUES ('Murlok');
+INSERT INTO employeeList VALUES ('Sylvanas Windrunner');
